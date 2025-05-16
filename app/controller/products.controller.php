96991f8860c9702d5e8bcc7d1f4fe1325ad0f293
Emma <64037956+mymy-cherrys@users.noreply.github.com>
2025-05-16 15:02:27 +0200

    Update products.controller.php

--- app/controller/products.controller.php
+++ app/controller/products.controller.php
@@ -1,13 +1,16 @@
 <?php
-require_once 'app/controller/controller.php';
-
+require_once 'app/controller/controller.php';     
+require_once 'app/model/products.model.php';       
 function generateProductPage() {
+    $bieres = getAllBieres(); 
+
     $data = [
         'page_title' => "Nos bières",
         'view' => 'app/view/product.view.php',
         'layout' => 'app/view/common/layout.php',
+        'bieres' => $bieres 
     ];
 
-    generatePage($data);
+    generatePage($data); 
 }
 ?>
