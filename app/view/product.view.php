07b5132bf8a8bbd2f58351c61aef3db8b1d9bbd7
Emma <64037956+mymy-cherrys@users.noreply.github.com>
2025-05-16 15:02:00 +0200

    Update product.view.php

--- app/view/product.view.php
+++ app/view/product.view.php
@@ -1,1 +1,12 @@
-<h1>Ceci est la page produit</h1>
+ <h1>Nos Bières</h1>
+
+    <?php foreach ($bieres as $biere): ?>
+        <div class="biere">
+            <h2><?= $biere['nom'] ?></h2>
+            <p class="prix">Prix : <?= $biere['prix'] ?></p>
+            <p><?= $biere['description'] ?></p>
+        </div>
+    <?php endforeach; ?>
+
+
+   
