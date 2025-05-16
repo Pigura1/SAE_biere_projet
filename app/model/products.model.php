d7811f94246ff7b45636d0d30532fe68057c96a3
Emma <64037956+mymy-cherrys@users.noreply.github.com>
2025-05-16 15:01:02 +0200

    Update products.model.php

--- app/model/products.model.php
+++ app/model/products.model.php
@@ -1,1 +1,14 @@
+ <?php
 
+
+function getAllBieres() {
+    return [
+        ["nom" => "Bière Blanche", "prix" => "3.50€", "description" => "Une bière légère et rafraîchissante."],
+        ["nom" => "Bière Ambrée", "prix" => "4.00€", "description" => "Une bière ambrée au goût riche."],
+        ["nom" => "Bière Brune", "prix" => "4.20€", "description" => "Une bière brune avec ."],
+        ["nom" => "IPA", "prix" => "4.50€", "description" => "Une bière houblonnée et fruitée."],
+        ["nom" => "Bière Blanche", "prix" => "3.80€", "description" => "Douce et légèrement acidulée."],
+        ["nom" => "Stout", "prix" => "4.30€", "description" => "Bière noire avec des notes de café."]
+    ];
+}
+?>
