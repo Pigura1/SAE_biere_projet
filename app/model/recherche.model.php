cf48446c467289568abb09bcf4d7616507ed6807
Pigura1 <C.victor2006@laposte.net>
2025-05-18 17:32:05 +0200

    ajout de la recherche + debut css trombinoscope

--- app/model/recherche.model.php
+++ app/model/recherche.model.php
@@ -4,7 +4,7 @@ require_once 'app/model/model.php';
 function getSearchedStudents(string $search)
 {
     $db = getDatabaseConnexion();
-    $stmt = $db->prepare("SELECT * FROM equipe WHERE prenom LIKE :search OR nom LIKE :search");
+    $stmt = $db->prepare("SELECT * FROM equipe WHERE Prenom LIKE :search OR Nom LIKE :search");
     $stmt->bindParam(':search', $search, PDO::PARAM_STR);
     $stmt->execute();
     return $stmt->fetchAll();
