29e2e86a8c606c2f893924a8057cd267afad7b2d
lrtheepeewielder <160053173+lrtheepeewielder@users.noreply.github.com>
2025-05-20 14:56:37 +0200

    Update footer.php

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -8,9 +8,10 @@
             <p>240 Rue de la Motte, 77550 Moissy-Cramayel, France</p>
             <div class="footer-socials">
 
+            <div class="logo-footer">
                 < href="#"><img src="public/images/Youtube_logo.png" alt="Youtube"></a>
                 <a href="#"><img src="public/images/instagramicone.png" alt="Instagram"
-                 ></a>
+                 ></a></div>
             
             </div>
             <p>&copy; <?= date("Y") ?> Cyb3r Heaven - Tous droits réservés</p>
