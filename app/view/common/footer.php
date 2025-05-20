776da506aed812ca897b27aa461248b77bd12f95
Luma <mymy-cherrys@users.noreply.github.com>
2025-05-20 14:38:34 +0200

    zaeaze

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -8,7 +8,7 @@
             <p>240 Rue de la Motte, 77550 Moissy-Cramayel, France</p>
             <div class="footer-socials">
                 <a href="#"><img src="public/images/Youtube_logo.png" alt="Youtube"></a>
-                <a href="#"><img src="public/images/qrcode.png" alt="Instagram"></a>
+                <a href="#"><img src="public/images/instagramicone.png" alt="Instagram"></a>
             </div>
             <p>&copy; <?= date("Y") ?> Cyb3r Heaven - Tous droits réservés</p>
         </div>
