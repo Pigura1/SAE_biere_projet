9f8381f6bff28d3037b72d4ecdd06d5cce083d53
Cyn1001 <tron30681@gmail.com>
2025-05-23 17:25:46 +0200

    acceuil
    caroussel

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -1,38 +1,61 @@
 <main>
-    <h2 class="hero glitch layers" data-text="EXAMPLE">
-  <span>Bienvenue sur le site officiel de Cyb3r Heaven !</span>
-</h2>
-<section id="caroussel">
-  <div id="slides">
-    <div class="bière">
+  <h2 class="hero glitch layers" data-text="EXAMPLE">
+    <span>Bienvenue sur le site officiel de Cyb3r Heaven !</span>
+  </h2>
+  <section id="caroussel">
+    <div id="slides">
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+      <div class="bière" style="background-image: url();">
+        <img src="" alt class="bières">
+        <div class="description">
+          <h3>...</h3>
+          <p>loremipsum</p>
+        </div>
+      </div>
+  </section>
 
+  <body>
+    <div class="video-container">
+      <video class="video" controls>
+        <source src="liendelavideo.mp4" type="video/mp4">
+        Votre navigateur ne prend pas en charge la vidéo.
+      </video>
     </div>
-     <div class="bière">
-      
-    </div>
-     <div class="bière">
-      
-    </div>
-     <div class="bière">
-      
-    </div>
-     <div class="bière">
-      
-    </div>
-     <div class="bière">
-      
-    </div>
-  </div>
-</section>
-
-<body>
-  <div class="video-container">
-    <video class="video" controls>
-      <source src="liendelavideo.mp4" type="video/mp4">
-      Votre navigateur ne prend pas en charge la vidéo.
-    </video>
-  </div>
-</body>
+  </body>
 </main>
 
 
