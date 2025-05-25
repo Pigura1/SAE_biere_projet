c5c8ea898956df670fbe246db445da1d8f73fb0c
Pigura1 <C.victor2006@laposte.net>
2025-05-25 18:45:08 +0200

    fix

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -13,8 +13,8 @@
     <p>Puis vient l’ébullition : on stérilise le moût et on ajoute notre hibiscus, notre rooibos et le houblon qui va apporter l’amertume, les arômes et aide à la conservation.</p>
     <img src="public/images/ebullition.png"/>
     <p>Après ça, on refroidit rapidement le moût pour pouvoir ajouter les levures sans les tuer. Celles-ci vont fermenter pendant plusieurs jours afin que les levures transforment les sucres en alcool et en gaz. </p>
-    <img src="public/images/densite1.jpg"/>
+    <img src="public/images/densite1.JPG"/>
     <img src="public/images/densite2.jpeg"/>
     <p>On a suivi cette étape de près jour après jour en mesurant la densité du moût jusqu’au jour de mise en bouteille où l’on a ajouté un peu de sucre pour relancer une fermentation en bouteille et obtenir une bière naturellement pétillante.</p>
-    <img src="public/images/bouteilles.jpg"/>
+    <img src="public/images/bouteilles.JPG"/>
 </div>
