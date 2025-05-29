3aab7edc5847f1c74d5606665ff244427bf28222
Cyn1001 <tron30681@gmail.com>
2025-05-29 20:26:35 +0200

    s

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -1,32 +1,51 @@
+<body>
+
+  <head>
+  </head>
+  <main>
+    <h2 class="hero glitch layers" data-text="EXAMPLE">
+      <span>Bienvenue sur le site officiel de Cyb3r Heaven !</span>
+    </h2>
 
 
-  <body>
-    <head>
-    </head>
-    <main>
-  <h2 class="hero glitch layers" data-text="EXAMPLE">
-    <span>Bienvenue sur le site officiel de Cyb3r Heaven !</span>
-  </h2>
-
-
-  <!-- 6 bières -->
-<div id="carousel">
-  <div class="slider">
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 1"></div>
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 2"></div>
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 3"></div>
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 4"></div>
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 5"></div>
-    <div class="slide"><img src="public/images/big/blonde.png" alt="Slide 6"></div>
-  </div>
-  </div>
+    <!-- 6 bières -->
+    <div id="carousel">
+      <div class="slider">
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p>
+          <img src="public/images/big/blonde.png" alt="Slide 1">
+        </div>
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p><img src="public/images/big/blonde.png" alt="Slide 2"></div>
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p><img src="public/images/big/blanche.png" alt="Slide 3"></div>
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p><img src="public/images/big/brune.png" alt="Slide 4"></div>
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p><img src="public/images/big/blonde.png" alt="Slide 5"></div>
+        <div class="slide">
+          <p id="description">
+            bière blonde
+          </p><img src="public/images/big/blonde.png" alt="Slide 6"></div>
+      </div>
+    </div>
     <div class="video-container">
       <video class="video" controls>
         <source src="liendelavideo.mp4" type="video/mp4">
         Votre navigateur ne prend pas en charge la vidéo.
       </video>
     </div>
-  </body>
+</body>
 </main>
 
 
