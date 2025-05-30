2cb374536a47e30d3cf0e87d60b176dd3bffbd19
Pigura1 <C.victor2006@laposte.net>
2025-05-30 17:40:10 +0200

    brassage

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -1,36 +1,57 @@
 <h1>Le processus du brassage de Cyb3r Heaven</h1>
 <div id="brassage">
+    <div id="introbras">
+    <h3 class="pbras">Voici le malt, l'ingrédient de base qui va servir au brassage de notre bière.</h3>
+    </div>
+    <div class="introbrasimg">
+    <img class="imgbras" src="public/images/malt.png"/>
+    </div>
+    <div id="etape1">
     
-    <h3>Voici le malt, l'ingrédient de base qui va servir au brassage de notre bière.</h3>
-    <img src="public/images/malt.png"/>
-
-    <h2>Etape 1</h2>
-    <p>En premier, on commence avec le concassage où l’on broie le malt pour libérer les sucres qui nourriront les levures. </p>
-    <img src="public/images/concassage1.jpg"/>
-    <img src="public/images/concassage2.png"/>
-
-    <h2>Etape 2</h2>
-    <p>Ensuite, c’est l’étape d’empâtage : on mélange le malt concassé avec de l’eau chaude.</p>
-    <img src="public/images/maltverseavant.jpeg"/>
-
+    <h2 class="etapetext">Etape 1</h2>
+    <p class="pbras" >En premier, on commence avec le concassage où l’on broie le malt pour libérer les sucres qui nourriront les levures. </p>
+    </div>
+    <div id="imgetape1">
+    <img class="imgbras" src="public/images/concassage1.jpg"/>
+    <img class="imgbras" src="public/images/concassage2.png"/>
+    </div>
     
-    <p>C’est là que les enzymes transforment l’amidon en sucres fermentés cibles. </p>
-    <img src="public/images/maltverse.png"/>
-
-    <h2>Etape 3</h2>
-    <p>Par la suite il faut filtrer ce mélange appelé moût, pour le séparer des résidus solides, qu’on appelle les drêches. </p>
-    <img src="public/images/filtrage.png"/>
-
-    <h2>Etape 4</h2>
-    <p>Puis vient l’ébullition : on stérilise le moût et on ajoute l'hibiscus, le rooibos et le houblon qui va apporter l’amertume et les arômes en aidant à la conservation. 
-    <p>Le moût est refroidit rapidement pour pouvoir ajouter les levures sans les tuer. Celles-ci vont fermenter pendant plusieurs jours afin que les levures transforment les sucres en alcool et en gaz.</p>
-    <img src="public/images/ebullition.png"/>
-
-    <h2>Etape 5</h2>
-    <p>Le groupe GB a suivi cette étape de près jour après jour en mesurant la densité du moût jusqu’au jour de mise en bouteille où ils ont ajouté un peu de sucre pour relancer une fermentation en bouteille et obtenir une bière naturellement pétillante.</p>
-    <img src="public/images/densite1.JPG"/>
-    <img src="public/images/densite2.jpeg"/>
-    <img src="public/images/bouteilles.JPG"/>
-
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/QI1AUaY0K9w?si=zGsQItX0TO4ox3Wz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <div id="etape2">
+    <h2 class="etapetext">Etape 2</h2>
+    <p class="pbras">Ensuite, c’est l’étape d’empâtage : on mélange le malt concassé avec de l’eau chaude.</p>
+    <p class="pbras">C’est là que les enzymes transforment l’amidon en sucres fermentés cibles. </p>
+    </div>
+    <div id="imgetape2">
+    <img class="imgbras" src="public/images/maltverseavant.jpeg"/>
+    
+    <img class="imgbras" src="public/images/maltverse.png"/>
+    </div>
+    
+    <div id="etape4">
+    <h2 class="etapetext">Etape 3</h2>
+    <p class="pbras">Par la suite il faut filtrer ce mélange appelé moût, pour le séparer des résidus solides, qu’on appelle les drêches. </p>
+    </div>
+    <div id="imgetape4">
+    <img class="imgbras" src="public/images/filtrage.png"/>
+    </div>
+    <div id="etape5">
+    <h2 class="etapetext">Etape 4</h2>
+    <p class="pbras">Puis vient l’ébullition : on stérilise le moût et on ajoute l'hibiscus, le rooibos et le houblon qui va apporter l’amertume et les arômes en aidant à la conservation. 
+    <p class="pbras">Le moût est refroidit rapidement pour pouvoir ajouter les levures sans les tuer. Celles-ci vont fermenter pendant plusieurs jours afin que les levures transforment les sucres en alcool et en gaz.</p>
+    </div>
+    <div id="imgetape5">
+    <img class="imgbras" src="public/images/ebullition.png"/>
+    </div>
+    <div id="etape6">
+    <h2 class="etapetext">Etape 5</h2>
+    <p class="pbras">Le groupe GB a suivi cette étape de près jour après jour en mesurant la densité du moût jusqu’au jour de mise en bouteille où ils ont ajouté un peu de sucre pour relancer une fermentation en bouteille et obtenir une bière naturellement pétillante.</p>
+    </div>
+    <div id="imgetape6">
+    <img class="imgbras" src="public/images/densite1.JPG"/>
+    <img class="imgbras" src="public/images/densite2.jpeg"/>
+    <img class="imgbras" src="public/images/bouteilles.JPG"/>
+    </div>
+    <div id="videobrassage">
+    <iframe id="videobras" width="560" height="315" src="https://www.youtube.com/embed/IdLZzX5zAUw?si=sW9THcmOJLqQJi1L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>    
 </div>
