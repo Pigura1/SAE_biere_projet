d0c1b75ead288d756426cfcbd53d5a66e3dc554f
Luma <mymy-cherrys@users.noreply.github.com>
2025-05-30 14:58:49 +0200

    footer mis a jour (jsuis goated)

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -36,27 +36,34 @@
 
 
 
-    <div class="footer-socials">
-      <span>Retrouvez-nous sur</span>
-
-  <a class="social-youtube" href="https://www.youtube.com/user/CyberPunkGame" target="_blank"><img src="public/images/youtube.svg" alt="Youtube"></a>
-  <a class="social-instagram" href="https://www.instagram.com/cyb3r_heaven?igsh=OHRkc2x5cDJqa3Zm&utm_source=qr" target="_blank"><img src="public/images/instagram.svg" alt="Instagram"></a>
+<div class="footer-socials">
+  <span>Retrouvez-nous sur</span>
   
+  <div class="socials-inline">
+    <a class="social-youtube" href="https://www.youtube.com/user/CyberPunkGame" target="_blank">
+      <img src="public/images/youtube.svg" alt="Youtube">
+    </a>
+    <a class="social-instagram" href="https://www.instagram.com/cyb3r_heaven?igsh=OHRkc2x5cDJqa3Zm&utm_source=qr" target="_blank">
+      <img src="public/images/instagram.svg" alt="Instagram">
+    </a>
+  </div>
+</div>
 
- <a href="index.php?route=home"><img src="public/images/logo.png" alt="Accueil" /></a>
-            <p>Brasserie artisanale</p>
-            <p>Université Paris-Est Créteil (UPEC), Bâtiment H</p>
-            <p>240 Rue de la Motte, 77550 Moissy-Cramayel, France</p>
-            <p>Tél : 01.10.01.10.01</p>
- </div>
+<div class="footer-logo-details">
+  <a href="index.php?route=home"><img src="public/images/small/groupe_E_anime.gif" alt="Accueil" /></a>
+  <p>Brasserie artisanale</p>
+  <p>Université Paris-Est Créteil (UPEC), Bâtiment H</p>
+  <p>240 Rue de la Motte, 77550 Moissy-Cramayel, France</p>
+  <p>Tél : 01.10.01.10.01</p>
+  <p>Mail : cyb3rheaven@brasserie.fr</p>
+</div>
 
-
-               <p> L'abus d'alcool est dangereux pour la santé, à consommer avec modération.</p>
-
+<p>L'abus d'alcool est dangereux pour la santé, à consommer avec modération.</p>
 
 <div class="footer-droits">
-            <p>&copy; <?= date("2025") ?> Cyb3rHeaven - Tous droits réservés</p>
-        </div>
+  <p>&copy; <?= date("2025") ?> Cyb3rHeaven - Tous droits réservés</p>
+</div>
+
     </footer>
 </body>
 </html>
