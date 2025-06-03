0084e11ebb686702176835eff3f718e324145c3b
Cyn1001 <tron30681@gmail.com>
2025-06-03 22:19:12 +0200

    zz

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -13,31 +13,38 @@
       <div class="slider">
         <div class="slide">
           <p id="description">
-            Cyb3r fact ! : lorempipsumol lopgoblo loponin likiue!
+            Cyb3r fact ! : Le nom de notre mascotte est Jerry !
           </p>
           <img src="public/images/big/blonde.png" alt="Slide 1">
         </div>
         <div class="slide">
           <p id="description">
-            bière blonde
-          </p><img src="public/images/big/blonde.png" alt="Slide 2"></div>
+            Cyb3r f4cts ! : Le 3 de Cyb3r réfère notre groupe de classe ! 
+          </p><img src="public/images/big/blanche.png" alt="Slide 2"></div>
         <div class="slide">
           <p id="description">
-            bière blonde
-          </p><img src="public/images/big/blanche.png" alt="Slide 3"></div>
+            Sc4ry Cyb3r f4cts... : 01101010 01100101 01110010 01110010 01111001 00001101 00001010
+          </p><img src="public/images/big/brune.png" alt="Slide 3"></div>
         <div class="slide">
           <p id="description">
-            bière blonde
-          </p><img src="public/images/big/brune.png" alt="Slide 4"></div>
+            Cyb3r f4cts ! : Nos bières ont été conçues à Night City ! (Cyberpunk2077) 
+          </p><img src="public/images/big/stout.png" alt="Slide 4"></div>
         <div class="slide">
           <p id="description">
-            bière blonde
-          </p><img src="public/images/big/blonde.png" alt="Slide 5"></div>
+            Cyb3r f4cts ! : Elles arrivent dans votre festival...
+          </p><img src="public/images/big/rousse.png" alt="Slide 5"></div>
         <div class="slide">
           <p id="description">
-            bière blonde
-          </p><img src="public/images/big/stout.png" alt="Slide 6"></div>
+            Cyb3r f4cts ! : C'est la dernière Cyb3r f4ct !
+          </p><img src="public/images/big/blonde.png" alt="Slide 6"></div>
       </div>
+    </div>
+    <div class="presentation">
+      <h2>Notre histoire</h2>
+      <p>Étudiants polyvalents, passionés, talentueux et excellent, nous vous accueillons dans notre monde paradisiaque.
+        <br>Conçues par nous même, nos bières du futur vous ferons voyagez dans un autre univers où tout est possible !
+        <br>Nous avions réunis nos différentes capacités, digitalement et physiquement, pour créer le produit parfait, et juste pour vous.
+      </p>
     </div>
     <div class="video-container">
       <video class="video" controls>
