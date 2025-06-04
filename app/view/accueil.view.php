3badc521ede9dd8421803f23a30b67e36674f229
Pigura1 <C.victor2006@laposte.net>
2025-06-04 22:09:34 +0200

    video

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -123,7 +123,7 @@
       </p>
     </div>
     <div class="video-container">
-     <iframe class="video" width="560" height="315" src="https://www.youtube.com/embed/ZmcT-9czHdo?si=Rs_xw6T6NvWWyMb9" 
+     <iframe class="video" width="660" height="515" src="https://www.youtube.com/embed/ZmcT-9czHdo?si=Rs_xw6T6NvWWyMb9" 
   title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
   referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
 </iframe>
